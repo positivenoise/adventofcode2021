cd2286b3f337a09423acea0665d876de9213b332
Brendan Muscat <brendan.muscat@gmail.com>
2021-12-08 17:00:06 +1100

    day 4 fix

--- day4/part1.py
+++ day4/part1.py
@@ -1,5 +1,3 @@
-lines = open("data.txt").read().splitlines()
-
 def calculate_result(board, i):
     result = 0
     for line in board:
@@ -8,56 +6,64 @@ def calculate_result(board, i):
     result = result * int(i)
     return result
 
+def process_into_array(data):
+    boards = []
+    board = []
+
+    for lines in data:
+        c = lines.split(' ')
+        if c != ['']:
+            t = [i for i in c if i != '']
+            board.append(t)
+        else:
+            boards.append(board)
+            board = []
+    boards.append(board)
+    return boards
 
 
-boards = []
-board = []
-for b in lines[2:]:
-    c = b.split(' ')
-    if c != ['']:
-        t = [i for i in c if i != '']
-        board.append(t)
-    else:
-        boards.append(board)
-        board = []
-boards.append(board)
-
-
-
-rotated_boards = []
-
-
-for b in boards:
-    new_board = [[],[],[],[],[]]
-    for l in b:
-        for x in range(0, len(l)):
-            new_board[x].append(l[x])
-    rotated_boards.append(new_board)
-
-
-
-
-
-for i in lines[0].split(','):
+def rotate_board(boards):
+    rotated_boards = []
     for b in boards:
+        new_board = [[],[],[],[],[]]
         for l in b:
-            if i in l:
-                l.remove(i)
-                if len(l) == 0:
-                    print("BINGO") 
-                    print(calculate_result(b, i))
-                    exit()
+            for x in range(0, len(l)):
+                new_board[x].append(l[x])
+        rotated_boards.append(new_board)
+    return rotated_boards
 
-    for b in rotated_boards:
-        for l in b:
-            if i in l:
-                l.remove(i)
-                if len(l) == 0:
-                    print("BINGO") 
-                    print(calculate_result(b, i))
-                    exit()           
+def remove_from_board(boards, num):
+    for board in boards:
+            for line in board:
+                if number in line:
+                    line.remove(num)
+    return boards
+
+def check_bingo(boards):
+    for board in boards:
+        for line in board:
+            if len(line) == 0:
+                return board
+    return False
 
 
+lines = open("data.txt").read().splitlines()
 
+input = lines[0].split(',')
+boards = process_into_array(lines[2:])
 
+rotated_boards = rotate_board(boards)
 
+for number in input:
+    remove_from_board(boards, number)
+    remove_from_board(rotated_boards, number)
+    result1 = check_bingo(boards)
+    result2 = check_bingo(rotated_boards)
+    if result1:
+        print("bingo")
+        print(calculate_result(result1, number))
+        break
+    if result2:
+        print("bingo")
+        print(calculate_result(result2, number))
+        break
--- day4/part2.py
+++ day4/part2.py
@@ -1,68 +1,114 @@
-lines = open("data.txt").read().splitlines()
-
 def calculate_result(board, i):
     result = 0
     for line in board:
         for num in line:
             result += int(num)
     result = result * int(i)
+
+    return result
+
+def process_into_array(data):
+    boards = []
+    board = []
+
+    for lines in data:
+        c = lines.split(' ')
+        if c != ['']:
+            t = [i for i in c if i != '']
+            board.append(t)
+        else:
+            boards.append(board)
+            board = []
+    boards.append(board)
+    return boards
+
+
+def rotate_board(boards):
+    rotated_boards = []
+    for b in boards:
+        new_board = [[],[],[],[],[]]
+        for l in b:
+            for x in range(0, len(l)):
+                new_board[x].append(l[x])
+        rotated_boards.append(new_board)
+    return rotated_boards
+
+def remove_from_board(boards, num):
+    for board in boards:
+            for line in boards[board]:
+                if number in line:
+                    line.remove(num)
+    return boards
+
+def check_bingo(boards):
+    for board in boards:
+        for line in boards[board]:
+            if len(line) == 0:
+                return board
+    return False
+
+def check_bingo_board(board):
+    for line in board:
+        if len(line) == 0:
+            return True
+    return False
+
+def add_index(boards):
+    result = {}
+    i = 1
+    for b in boards:
+        result[i] = b
+        i+= 1
     return result
 
 
+lines = open("data.txt").read().splitlines()
 
-boards = []
-board = []
-for b in lines[2:]:
-    c = b.split(' ')
-    if c != ['']:
-        t = [i for i in c if i != '']
-        board.append(t)
-    else:
-        boards.append(board)
-        board = []
-boards.append(board)
+input = lines[0].split(',')
+boards = process_into_array(lines[2:])
+
+rotated_boards = rotate_board(boards)
 
 
 
-rotated_boards = []
+boards = add_index(boards)
+rotated_boards = add_index(rotated_boards)
+
+for number in input:
+    remove_from_board(boards, number)
+    remove_from_board(rotated_boards, number)
+    result1 = check_bingo(boards)
+    result2 = check_bingo(rotated_boards)
+
+    if result1 and len(boards) == 1:
+        print("bingo")
+        print(calculate_result(boards[result1], number))
+        break
+
+    elif result2 and len(rotated_boards) == 1:
+        print("bingo")
+        print(calculate_result(rotated_boards[result2], number))
+        break
 
 
-for b in boards:
-    new_board = [[],[],[],[],[]]
-    for l in b:
-        for x in range(0, len(l)):
-            new_board[x].append(l[x])
-    rotated_boards.append(new_board)
 
-
-
-
-
-for i in lines[0].split(','):
+    new_boards = {}
+    new_rotated_boards = {}
     for b in boards:
-        for l in b:
-            if i in l:
-                l.remove(i)
-                if len(l) == 0:
-                    index_n = boards.index(b)
-                    del boards[index_n]
-                    del rotated_boards[index_n]
-                    if len(boards) == 1 or len(rotated_boards) == 1:
-                        print(calculate_result(boards[0], i))
-                        exit()
+        if not check_bingo_board(boards[b]):
+            new_boards[b] = boards[b]
+            new_rotated_boards[b] = rotated_boards[b]
+    boards = new_boards
+    rotated_boards = new_rotated_boards
 
+
+    new_boards = {}
     for b in rotated_boards:
-        for l in b:
-            if i in l:
-                l.remove(i)
-                if len(l) == 0:
-                    index_n = rotated_boards.index(b)
-                    del boards[index_n]
-                    del rotated_boards[index_n]
-                    if len(boards) == 1 or len(rotated_boards) == 1:
-                        print(calculate_result(rotated_boards[0], i))
-                        exit()       
-
+        if not check_bingo_board(rotated_boards[b]):
+            new_boards[b] = boards[b]
+            new_rotated_boards[b] = rotated_boards[b]
+    boards = new_boards
+    rotated_boards = new_rotated_boards
 
 
 
